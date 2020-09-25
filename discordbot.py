e314ac9f6dc75b60fbe6abf39bd4df8a647698c6
aigakusyu <71875416+aigakusyu@users.noreply.github.com>
2020-09-26 00:22:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,53 +1,16 @@
-# インストールした discord.py を読み込む
-import discord
-import pandas as pd
-import random
-import codecs
-siritori =""
+from discord.ext import commands
+import os
+import traceback
 
-url = 'https://raw.githubusercontent.com/aigakusyu/discordpy-startup/master/s_hyou.csv'
-df = pd.read_csv(url, error_bad_lines=False)
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
-with codecs.open('df', "r", "UTF-8", "ignore") as file:
-    siritori = pd.read_table(file, delimiter=",")
-
-# 自分のBotのアクセストークンに置き換えてください
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
-
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
-
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-
-    def check(msg):
-        return msg.author == message.author
-
-    if message.content.startswith("/s start"):
-
-        await message.channel.send("しりとりスタート！　しりとり")
-
-        while True:
-
-            wait_message = await client.wait_for("message", check=check)
-
-            i = random.randint(0,100)
-
-            await message.channel.send(siritori[wait_message.content[-1]][i])
-
-            if wait_message.content[-1] == "ん":
-                print("最後が「ん」だよ！！！！！！")
-                break
-
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
+@bot.event
+async def on_command_error(ctx, error):
+    orig_error = getattr(error, "original", error)
+    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    await ctx.send(error_msg)
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+bot.run(token)
